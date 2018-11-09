d53dff9a61f2aa5dcb138e06ac9dfc07ab340d1c
berneskaracay <berneskaracay@gmail.com>
2018-11-09 12:34:23 -0600

    cms data merged opioid claim per person ggplot added

---
+++ README.md
@@ -0,0 +1,13 @@
+### Datasets 
+Retrieved from https://www.kaggle.com/apryor6/us-opiate-prescriptions
+- opioids data
+- prescriber-info data 
+- overdoses
+
+### Questions:
+1. Explore the dataset. Consider plotting the number of types of opioids by state and number of opioid prescribers by state. Understand the *most prevalent* types of opioids prescribed. Explore the states with the highest prevalence of fatal opioid overdoses. How does this compare to total overdoses? Is there a correlation between type of opioids prescribed and overdose deaths? What patterns emerge? Use different types of plots to visualize your data (histogram, bee swarm, box plot, etc...).  
+
+2. Test the assumption of the Pareto effect on opioid prescribing using the dataset (i.e., 20% of prescribers provide 80% of the opioids).  
+
+3. Build a model to predict the number of fatal opioid overdoses. Consider building the model for a specific state, but that’s not necessary. Be creative—consider crossing the raw features that are available and creating new features (i.e., total type of opioids). Socioeconomics are often a driving factor for health outcomes. Can you find another dataset that provides information at the state level and can be used in your model? Can you perform tests to explain the variability in your outcome (i.e, effect estimation)?
+
--- opioids.R
+++ opioids.R
@@ -6,7 +6,7 @@
 rm(list=ls())
 
 ### set working directory###
-setwd("C:\\Users\\karacb1\\Desktop\\us-opiate-prescriptions")
+setwd("C:\\Users\\karacb1\\Desktop\\data-question-5-opioids-navy-cats")
 
 
 
@@ -18,13 +18,14 @@ library(rms)
 library(MASS)
 library(survival)
 library(plyr)
-
+library(tidyverse)
 
 ### read raw data sets ###
 
 opioids=read.csv('opioids.csv', stringsAsFactors=FALSE)
 overdoses=read.csv('overdoses.csv', stringsAsFactors=FALSE)
 prescriber=read.csv('prescriber-info.csv', stringsAsFactors=FALSE)
+cms=read.csv('cms.csv', stringsAsFactors=FALSE)
 
 ### check identifier variable ###
 table(overdoses$Abbrev,exclude=NULL)
@@ -36,8 +37,10 @@ overdoses$death_percent<-(overdoses$Deaths/overdoses$Population)*(100)
 overdoses$state_code <- factor(overdoses$Abbrev, levels=unique(as.character(overdoses$Abbrev)))
 
 
+data_1 <- merge(x=overdoses, y=prescriber, by.x='Abbrev',by.y='State', all.x=TRUE)
+data_combined <- merge(x=cms, y=data_1, by.x='NPI',by.y='NPI', all.y=TRUE)
 
-data_combined <- merge(x=overdoses, y=prescriber, by.x='Abbrev',by.y='State', all=TRUE)
+table(data_1$State,exclude=NULL)
 
 table(overdoses$death_percent,exclude=NULL)
 
@@ -51,6 +54,35 @@ table(overdoses$state_code,exclude=NULL)
 class(data_combined$Deaths)
 class(data_combined$state_code)
 class(data_combined$state_code)
+data_combined$ones<-1
+
+
+
+
+#z <- data_combined[!duplicated(data_combined$state_code),]
+
+#liste=c("Abbrev","ones","State","Population","Deaths","death_percent","state_code","NPI","Gender","Credentials","Specialty","Opioid.Prescriber")
+#count_data <- data_combined[liste]
+
+
+count_dr_group<-data_combined %>%filter(Opioid.Prescriber==1)%>% 
+  group_by(Specialty.Description) %>% 
+  summarise(agg =sum(Opioid.Claim.Count))
+
+count_state_group<-data_combined %>%filter(Opioid.Prescriber==1)%>% 
+  group_by(State) %>% 
+  summarise(agg =sum(Opioid.Claim.Count))
+
+count_state_percent_group<-data_combined %>%filter(Opioid.Prescriber==1)%>% mutate(Opioid.Claim.Count = Opioid.Claim.Count/Population, na.rm = TRUE)%>%
+  group_by(State) %>% 
+  summarise(agg =sum(Opioid.Claim.Count))
+
+
+
+#bernes <- merge(x=count_dr, y=data_combined, by.x='State',by.y='State', all=TRUE)
+
+mysample <- data_combined[sample(1:nrow(data_combined), 50,
+                          replace=FALSE),]
 
 Sweave("opioids_report.Rnw", output=paste0("DQ4", ".tex"))
 
