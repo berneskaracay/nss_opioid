96b686d39954a95f4e2ba666fd63ca4d5a147ffa
berneskaracay <berneskaracay@gmail.com>
2018-11-22 09:30:15 -0600

    pareto effect done

--- opioids.R
+++ opioids.R
@@ -6,7 +6,7 @@
 rm(list=ls())
 
 ### set working directory###
-setwd("C:\\Users\\karacb1\\Desktop\\data-question-5-opioids-navy-cats")
+setwd("C:\\Users\\karacb1\\Desktop\\nss_opioid")
 # bernes
 
 
@@ -19,13 +19,17 @@ library(MASS)
 library(survival)
 library(plyr)
 library(tidyverse)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
 
 ### read raw data sets ###
 
-opioids=read.csv('opioids.csv', stringsAsFactors=FALSE)
-overdoses=read.csv('overdoses.csv', stringsAsFactors=FALSE)
-prescriber=read.csv('prescriber-info.csv', stringsAsFactors=FALSE)
-cms=read.csv('cms.csv', stringsAsFactors=FALSE)
+opioids=read.csv('data/opioids.csv', stringsAsFactors=FALSE)
+overdoses=read.csv('data/overdoses.csv', stringsAsFactors=FALSE)
+prescriber=read.csv('data/prescriber-info.csv', stringsAsFactors=FALSE)
+cms=read.csv('data/cms.csv', stringsAsFactors=FALSE)
+pres_14=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2014.csv', stringsAsFactors=FALSE)
 
 ### check identifier variable ###
 table(overdoses$Abbrev,exclude=NULL)
@@ -83,6 +87,192 @@ count_state_percent_group<-data_combined %>%filter(Opioid.Prescriber==1)%>% muta
 
 mysample <- data_combined[sample(1:nrow(data_combined), 50,
                           replace=FALSE),]
+
+
+#############################################################################
+#################### prescriber pareto analysis #############################
+#############################################################################
+
+colnames(pres_14)[colnames(pres_14)=="NPPES.Provider.State"]<-"State"
+table(pres_14["State"],exclude = NULL)
+not_state=c('AA','GU','AE','ZZ',"XX","AP","AS","GU","MP","PR")
+pres_14=pres_14[pres_14$State %nin% not_state,]
+table(pres_14["State"],exclude = NULL)
+colnames(pres_14)[colnames(pres_14)=="NPPES.Provider.Last.Org.Name"]<-"last_name"
+colnames(pres_14)[colnames(pres_14)=="NPPES.Provider.First.Name"]<-"first_name"
+colnames(pres_14)[colnames(pres_14)=="Total.Claim.Count"]<-"total_claim"
+colnames(pres_14)[colnames(pres_14)=="Opioid.Prescribing.Rate"]<-"opioid_rate"
+colnames(pres_14)[colnames(pres_14)=="Opioid.Claim.Count"]<-"opioid_claim"
+colnames(pres_14)[colnames(pres_14)=="NPPES.Provider.Zip.Code"]<-"zipcode"
+colnames(pres_14)[colnames(pres_14)=="Specialty.Description"]<-"Specialty"
+
+
+glimpse(pres_14)
+filter(pres_14,NPI==1518048750)
+
+
+################## Question 2 ################################
+
+
+
+#######2013##############
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2013.csv', stringsAsFactors=FALSE)
+
+bernes<-opioids %>%
+  dplyr::summarize(Opioids_sum= sum(`Opioid.Claim.Count`,na.rm=TRUE),
+                   Total_claims = sum(`Total.Claim.Count`,na.rm=TRUE)) %>% 
+  mutate(limit = Opioids_sum*0.8)%>%ungroup
+
+opioids$limit<-bernes$limit
+opioids<-arrange(opioids, desc(Opioid.Claim.Count))
+
+
+
+opioids <-opioids%>%mutate(cum_claim = cumsum(`Opioid.Claim.Count`))
+opioids$passed_limit<-0
+opioids$passed_limit[opioids["cum_claim"]>opioids["limit"]]<-1
+
+
+first_index<-min(which(opioids$passed_limit == 1))
+pareto_2013=first_index/nrow(opioids)*100
+
+top_10_opioids_prescriber_2013 <- opioids %>% 
+  top_n(n = 20, wt = Opioid.Claim.Count)
+
+names_2013<-c(top_10_opioids_prescriber_2013$NPI)
+
+
+#######2014##############
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2014.csv', stringsAsFactors=FALSE)
+
+
+bernes<-opioids %>%
+  dplyr::summarize(Opioids_sum= sum(`Opioid.Claim.Count`,na.rm=TRUE),
+                   Total_claims = sum(`Total.Claim.Count`,na.rm=TRUE)) %>% 
+  mutate(limit = Opioids_sum*0.8)%>%ungroup
+
+opioids$limit<-bernes$limit
+opioids<-arrange(opioids, desc(Opioid.Claim.Count))
+
+
+
+opioids <-opioids%>%mutate(cum_claim = cumsum(`Opioid.Claim.Count`))
+opioids$passed_limit<-0
+opioids$passed_limit[opioids["cum_claim"]>opioids["limit"]]<-1
+
+
+first_index<-min(which(opioids$passed_limit == 1))
+pareto_2014=first_index/nrow(opioids)*100
+
+top_10_opioids_prescriber_2014 <- opioids %>% 
+  top_n(n = 20, wt = Opioid.Claim.Count)
+
+names_2014<-c(top_10_opioids_prescriber_2014$NPI)
+
+
+#######2015##############
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2015.csv', stringsAsFactors=FALSE)
+
+bernes<-opioids %>%
+  dplyr::summarize(Opioids_sum= sum(`Opioid.Claim.Count`,na.rm=TRUE),
+                   Total_claims = sum(`Total.Claim.Count`,na.rm=TRUE)) %>% 
+  mutate(limit = Opioids_sum*0.8)%>%ungroup
+
+opioids$limit<-bernes$limit
+opioids<-arrange(opioids, desc(Opioid.Claim.Count))
+
+
+
+opioids <-opioids%>%mutate(cum_claim = cumsum(`Opioid.Claim.Count`))
+opioids$passed_limit<-0
+opioids$passed_limit[opioids["cum_claim"]>opioids["limit"]]<-1
+
+
+first_index<-min(which(opioids$passed_limit == 1))
+pareto_2015=first_index/nrow(opioids)*100
+
+top_10_opioids_prescriber_2015 <- opioids %>% 
+  top_n(n = 20, wt = Opioid.Claim.Count)
+
+names_2015<-c(top_10_opioids_prescriber_2015$NPI)
+
+#######2016##############
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2016.csv', stringsAsFactors=FALSE)
+
+bernes<-opioids %>%
+  dplyr::summarize(Opioids_sum= sum(`Opioid.Claim.Count`,na.rm=TRUE),
+                   Total_claims = sum(`Total.Claim.Count`,na.rm=TRUE)) %>% 
+  mutate(limit = Opioids_sum*0.8)%>%ungroup
+
+opioids$limit<-bernes$limit
+opioids<-arrange(opioids, desc(Opioid.Claim.Count))
+
+
+
+opioids <-opioids%>%mutate(cum_claim = cumsum(`Opioid.Claim.Count`))
+opioids$passed_limit<-0
+opioids$passed_limit[opioids["cum_claim"]>opioids["limit"]]<-1
+
+
+first_index<-min(which(opioids$passed_limit == 1))
+pareto_2016=first_index/nrow(opioids)*100
+
+top_10_opioids_prescriber_2016 <- opioids %>% 
+  top_n(n = 20, wt = Opioid.Claim.Count)
+
+names_2016<-c(top_10_opioids_prescriber_2016$NPI)
+
+
+top_dr<-Reduce(intersect, list(names_2013, names_2014,names_2015,names_2016))
+
+
+print(top_dr)
+
+######################tOP PRESCRIBERS###########################
+
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2016.csv', stringsAsFactors=FALSE)
+
+top_2016<-subset(opioids,NPI  %in% top_dr)
+top_2016 <- top_2016 %>% 
+  mutate(year = 2016) 
+
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2015.csv', stringsAsFactors=FALSE)
+
+top_2015<-subset(opioids,NPI  %in% top_dr)
+top_2015 <- top_2015 %>% 
+  mutate(year = 2015) 
+
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2014.csv', stringsAsFactors=FALSE)
+
+top_2014<-subset(opioids,NPI  %in% top_dr)
+top_2014 <- top_2014 %>% 
+  mutate(year = 2014) 
+
+opioids=read.csv('data/Medicare_Part_D_Opioid_Prescriber_Summary_File_2013.csv', stringsAsFactors=FALSE)
+
+top_2013<-subset(opioids,NPI  %in% top_dr)
+top_2013 <- top_2013 %>% 
+  mutate(year = 2013) 
+
+cms_combined <- bind_rows(top_2013,top_2014,top_2015,top_2016)
+
+ggplot(cms_combined, aes(x=year,
+                         y=Opioid.Claim.Count)) + 
+  facet_wrap(~ `NPI`) +
+  geom_col() +
+  xlab("Generic Drug Name") +
+  ylab("Opioid Prescription Rate") +
+  ggtitle("2013 Opioid Prescription Rate", subtitle = "For Top 5 States by Drug Overdose Rate") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust = 0.25))
+
+
+ggplot(cms_combined, aes(x=reorder(year),
+                                     y=Opioid.Claim.Count)) + 
+  geom_bar(stat = "identity") +
+  labs(x = "State", y = "Median Prescription Rate", title = "Median Opioid Prescription Rates",
+       subtitle = "2013 - 2016")
+
+
 
 Sweave("opioids_report.Rnw", output=paste0("DQ5", ".tex"))
 
